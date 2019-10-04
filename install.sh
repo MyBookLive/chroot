246fe555800d9acf8c58cc11d471c45a326931a4
Alexey <dm38@users.noreply.github.com>
2019-10-04 13:01:50 +0300

    Update url

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ else
 fi
 iSystem=jessie
 chrootBaseDir=/DataVolume/$chrootDir
-debootstrapPkgName=debootstrap_1.0.87_all.deb
+debootstrapPkgName=debootstrap_1.0.89~bpo8+1_all.deb
 projectURL=https://github.com/MyBookLive/chroot/raw/master
 isServicesInstalled=no
 WGET="wget --no-check-certificate -q -O"
@@ -52,7 +52,7 @@ dpkg -i /tmp/$debootstrapPkgName
 rm -f /tmp/$debootstrapPkgName
 echo -e $INFO Preparing a new Debian $iSystem chroot file base. Please, be patient,
 echo -e $INFO may takes a long time on low speed connection...
-debootstrap --no-check-gpg --no-check-certificate --variant=minbase --exclude=yaboot,udev,dbus,systemd --include=locales,mc,aptitude,wget,dialog,apt-utils,sysvinit-core,sysvinit,sysvinit-utils $iSystem $chrootBaseDir ftp://ftp.ru.debian.org/debian
+debootstrap --no-check-gpg --no-check-certificate --variant=minbase --exclude=yaboot,udev,dbus,systemd --include=locales,mc,aptitude,wget,dialog,apt-utils,sysvinit-core,sysvinit,sysvinit-utils $iSystem $chrootBaseDir http://archive.debian.org/debian-archive/debian/
 echo "share:x:1000:root,www-data,daapd" >> $chrootBaseDir/etc/group
 cat > $chrootBaseDir/usr/sbin/policy-rc.d <<EOF
 #!/bin/sh
